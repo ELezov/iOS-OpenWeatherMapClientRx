08c84e9c6817af0d60b2eff14bdd80bab58010fb
Eugene <ELezov@stud.kantiana.ru>
2017-11-06 10:42:40 +0200

    Initial Commit

---
+++ iOS-OpenWeatherMapClientRx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-OpenWeatherMapClientRx
+//
+//  Created by Nikolay on 06.11.17.
+//  Copyright © 2017 Eugene Lezov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
